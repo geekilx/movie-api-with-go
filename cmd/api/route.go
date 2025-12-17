2b851c1afc891f9f06600b27df1aac96b0b2411f
ilx <iliakeshavarz23@gmail.com>
2025-12-17 20:02:18 +0330

    added POST /v1/users/authentication in order to authenticate user by email and password and generate a 24 hours temporary token

--- cmd/api/route.go
+++ cmd/api/route.go
@@ -22,6 +22,7 @@ func (app *application) route() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.createUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/users/authentication", app.userAuthenticationHandler)
 
 	return app.panicRecover(app.rateLimit(router))
 
