9ca42a43e7da8b6febcc30b345a73b47dfb7e0d5
ilx <iliakeshavarz23@gmail.com>
2025-12-23 08:48:59 +0330

    using addForUser permission method in order to add movies:read permission by default to evry new users

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -55,6 +55,12 @@ func (app *application) createUserHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+	err = app.models.Permissions.AddForUser(user.ID, "movies:read")
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	token, err := app.models.Tokens.New(int64(user.ID), 72*time.Hour, data.ScopeActivation)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
